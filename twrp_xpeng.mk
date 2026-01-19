387be55d6fd4fc4a1f097be9fedf703cf7d4a914
Jotanh <jotanh@jotanic.xyz>
2026-01-19 21:14:23 +0000

    xpeng: Add Maintainer Status and Switch to TWRP Vendor

--- twrp_xpeng.mk
+++ twrp_xpeng.mk
@@ -13,9 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit from xpeng device
 $(call inherit-product, device/motorola/xpeng/device.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := xpeng
