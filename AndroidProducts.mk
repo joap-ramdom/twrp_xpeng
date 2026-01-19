91980ebe99365e24d8e6e5c9488c3ea620609a16
Jotanh <jotanh@jotanic.xyz>
2026-01-19 21:26:05 +0000

    Xpeng: Revert to omni.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_xpeng.mk
+    $(LOCAL_DIR)/omni_xpeng.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_xpeng-user \
-    twrp_xpeng-userdebug \
-    twrp_xpeng-eng
+    omni_xpeng-user \
+    omni_xpeng-userdebug \
+    omni_xpeng-eng
